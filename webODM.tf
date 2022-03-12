03b49acba72822084ef81f5be76055e9d1345f85
Chris Kendrick <kendrickcc@gmail.com>
2022-03-12 14:47:03 -0600

    reformat output

--- webODM.tf
+++ webODM.tf
@@ -232,12 +232,12 @@ resource "azurerm_linux_virtual_machine" "nodeodm" {
 #-------------------------------
 # Outputs
 #-------------------------------
-output "azurerm_public_ip" {
+output "WebODM_public_ip_port_8000" {
   value = azurerm_linux_virtual_machine.webodm.*.public_ip_addresses
 }
-output "nodeodm_private_ip_addresses" {
-  value = azurerm_linux_virtual_machine.nodeodm.*.private_ip_addresses
-}
-output "webodm_private_ip_addresses" {
+output "clusterodm_private_ip_addresses_port_8001" {
   value = azurerm_linux_virtual_machine.webodm.*.private_ip_addresses
+}
+output "nodeodm_private_ip_addresses_port_3000" {
+  value = azurerm_linux_virtual_machine.nodeodm.*.private_ip_addresses
 }
