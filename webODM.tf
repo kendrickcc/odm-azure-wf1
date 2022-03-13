a41bd192dcae19218def0b0c6e6f7486c52491c2
Chris Kendrick <kendrickcc@gmail.com>
2022-03-12 20:47:43 -0600

    disable port 22

--- webODM.tf
+++ webODM.tf
@@ -125,7 +125,7 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tags                = merge(local.common_tags)
-  # /* when needed to connect to VM 
+   /* when needed to connect to VM 
   security_rule {
     name                       = "SSH"
     priority                   = 300
